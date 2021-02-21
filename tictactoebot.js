ff003391b22e799a00b7d7a0e576406f320537be
Hakob Sargsyan <hsargsyan395@gmail.com>
2021-02-22 00:48:59 +0400

    m

--- tictactoebot.js
+++ tictactoebot.js
@@ -9,4 +9,23 @@ addEventListener('DOMContentLoaded', () => {
     document.head.append(icon);
     document.title = 'Tic-Tac-Toe';
     document.getElementsByTagName('table')[1].style.display = 'none';
+    for (let i = 0; i < 3; i++) {
+        let button = document.getElementsByTagName('button')[i];
+        if (window.matchMedia('(max-width: 1600px)').matches) {
+            button.style.color = 'black';
+            button.style.outline = 'none';
+            button.style.fontSize = '30px';
+            button.style.height = '40px'
+            button.style.margin = '8px';
+            button.style.cursor = 'pointer';
+        }
+        if (window.matchMedia('(min-width: 1601px)').matches) {
+            button.style.color = 'black';
+            button.style.outline = 'none';
+            button.style.fontSize = '40px';
+            button.style.height = '50px'
+            button.style.margin = '10px';
+            button.style.cursor = 'pointer';
+            }
+    }
 })
